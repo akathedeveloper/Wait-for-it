b554ef7b5d33ee11b6389af01aff6af01f648f0b
akathedeveloper <114748865+akathedeveloper@users.noreply.github.com>
2024-07-07 19:39:07 +0530

    Some Part of Navbar Done

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link"
+import MobileMenu from "./MobileMenu"
+
+const Navbar = () => {
+  return (
+    <div className="h-24 flex items-center justify-between">
+        {/*Left*/}
+        <div className="">
+            <Link href="/" className="font-bold text-xl text-blue-600">Social Sphere</Link>
+        </div>
+        {/*Center*/}
+        <div className="">
+        
+        </div>
+        {/*Right*/}
+        <div className="">
+            <MobileMenu/>
+        </div>
+    <div className="">
+        
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
