3b9bbdcaa86f1da365f557c75398f944bb32bff9
akathedeveloper <114748865+akathedeveloper@users.noreply.github.com>
2024-07-13 21:21:20 +0530

    Posts Done

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -22,10 +22,35 @@ const Post = () => {
             </p>
         </div>
         {/*INTERACTION*/}
-        <div className="">
-            
+        <div className="flex items-center justify-between text-sm mt-4">
+            <div className="flex gap-8">
+                <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+                <Image src="/like.png" alt="" width={16} height={16} className="cursor-pointer"/>
+                <span className="text-gray-300">|</span>
+                <span className="text-gray-500">123
+                    <span className="hidden md:inline"> Likes</span>
+                </span>
+                </div>
+
+                <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+                <Image src="/comment.png" alt="" width={16} height={16} className="cursor-pointer"/>
+                <span className="text-gray-300">|</span>
+                <span className="text-gray-500">123 
+                    <span className="hidden md:inline"> Comment</span>
+                </span>
+                </div>
+            </div>
+            <div className="">
+            <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+                <Image src="/share.png" alt="" width={16} height={16} className="cursor-pointer"/>
+                <span className="text-gray-300">|</span>
+                <span className="text-gray-500">123 
+                    <span className="hidden md:inline"> Share</span>
+                </span>
+                </div>
+            </div>
         </div>
-        {/*USER*/}
+        
     </div>
   )
 }
