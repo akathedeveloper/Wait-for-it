315e57d0a8462e4bb2b3f1e365ea43cf437bbea2
akathedeveloper <114748865+akathedeveloper@users.noreply.github.com>
2024-07-13 20:15:25 +0530

    Stories Added

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,6 +1,18 @@
+import 
+
+
 const AddPost = () => {
   return (
-    <div>AddPost</div>
+    <div className="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
+      {/*AVATAR*/}
+      <Image className=""></Image>
+      {/*POST*/}
+      <div className="">
+        {/*TEXT INPUT*/}
+        <div className=""></div>
+        <div className=""></div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -2,10 +2,35 @@ import Image from "next/image"
 
 const Stories = () => {
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-sm">
-      <div className="flex gap-8">
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
+      <div className="flex gap-8 w-max">
         <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image src="" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+          <Image src="https://images.pexels.com/photos/18939401/pexels-photo-18939401.jpeg?cs=srgb&dl=pexels-juan-fam-3015522-18939401.jpg&fm=jpg" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+          <span className="font-medium">Adhiraj</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://images.pexels.com/photos/18939401/pexels-photo-18939401.jpeg?cs=srgb&dl=pexels-juan-fam-3015522-18939401.jpg&fm=jpg" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+          <span className="font-medium">Adhiraj</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://images.pexels.com/photos/18939401/pexels-photo-18939401.jpeg?cs=srgb&dl=pexels-juan-fam-3015522-18939401.jpg&fm=jpg" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+          <span className="font-medium">Adhiraj</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://images.pexels.com/photos/18939401/pexels-photo-18939401.jpeg?cs=srgb&dl=pexels-juan-fam-3015522-18939401.jpg&fm=jpg" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+          <span className="font-medium">Adhiraj</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://images.pexels.com/photos/18939401/pexels-photo-18939401.jpeg?cs=srgb&dl=pexels-juan-fam-3015522-18939401.jpg&fm=jpg" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+          <span className="font-medium">Adhiraj</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://images.pexels.com/photos/18939401/pexels-photo-18939401.jpeg?cs=srgb&dl=pexels-juan-fam-3015522-18939401.jpg&fm=jpg" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+          <span className="font-medium">Adhiraj</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://images.pexels.com/photos/18939401/pexels-photo-18939401.jpeg?cs=srgb&dl=pexels-juan-fam-3015522-18939401.jpg&fm=jpg" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+          <span className="font-medium">Adhiraj</span>
         </div>
       </div>
     </div>
